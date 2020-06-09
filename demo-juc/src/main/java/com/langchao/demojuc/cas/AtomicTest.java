e5b08cd7f5a5cf52fffacb67df22474c6ecc06de
liyuan0514 <724837404@qq.com>
2020-06-09 21:26:20 +0800

    Merge branch 'master' of D:\code\wave with conflicts.

---
+++ demo-juc/src/main/java/com/langchao/demojuc/cas/AtomicTest.java
@@ -0,0 +1,8 @@
+package com.langchao.demojuc.cas;
+
+/**
+ * @author liyuan
+ * @create 2020-06-06-21:59
+ */
+public class AtomicTest {
+}
--- wave-service-mall/src/main/java/com/langchao/waveservicemall/mapper/UserInfoMapper.java
+++
@@ -1,8 +0,0 @@
-package com.langchao.waveservicemall.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.langchao.waveservicemall.pojo.UserInfo;
-
-public interface UserInfoMapper extends BaseMapper<UserInfo> {
-}
